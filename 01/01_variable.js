63c452a077217422bb016bee25f2ec7b87313db9
Rushikesh Mangrule <you@example.com>
2025-11-21 18:04:05 +0530

    variables and how any type  works

---
+++ 01/01_variable.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = {
+    name: "rushikesh",
+    age: 20,
+};
+var greetings = "Hello Rushikesh";
+console.log(greetings);
+console.log(user.name);
+//number 
+var userId;
+userId = 3323144.24;
+let type = userId.toFixed();
+console.log(typeof type);
+
+console.log(userId.toFixed());
+
+console.log(typeof userId);
+
+// boolean
+var isUserLoggedIn = false;
+console.log(typeof isUserLoggedIn);
---
+++ 01/01_variable.ts
@@ -0,0 +1,25 @@
+let user: any = {
+    name: "rushikesh",
+    age : 20,
+}
+
+let greetings: string = "Hello Rushikesh"
+
+console.log(greetings);
+console.log(user.name);
+
+//number 
+
+let userId: number;
+
+userId = 3323144;
+userId.toFixed()
+
+// boolean
+
+let isUserLoggedIn: boolean = false;
+
+console.log(typeof isUserLoggedIn);
+
+
+export {}
