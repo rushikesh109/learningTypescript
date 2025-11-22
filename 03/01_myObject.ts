65285d3ab3718f521ae2af542f906d5a4e608f62
Rushikesh Mangrule <you@example.com>
2025-11-22 19:04:19 +0530

    bad behaviour of object

---
+++ 03/01_myObject.ts
@@ -0,0 +1,30 @@
+const user = {
+  name: "rushi",
+  email: "r@r.com",
+  isActive: true,
+};
+
+function createUser(): { name: string; email: string; isActive: boolean } {
+  return {
+    name: "rushi",
+    email: "rushi@gmail.com",
+    isActive: true,
+  };
+}
+
+
+function createCourse(user: {
+  name: string;
+  isPaid: boolean;
+//   email: string;
+}) {}
+
+let newUser = {
+  name: "rushi",
+  isPaid: false,
+//   email: "rushi@gmail.com",
+};
+
+createCourse(newUser);
+
+export {};
