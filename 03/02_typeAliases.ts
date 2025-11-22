9898c1049dfc8ec9d29eaa41e7eec14a7bcb1942
Rushikesh Mangrule <you@example.com>
2025-11-22 19:53:31 +0530

    readonly and optional

--- 03/02_typeAliases.ts
+++ 03/02_typeAliases.ts
@@ -1,16 +1,58 @@
+// type User = {
+//     name: string;
+//     email: string;
+//     isActive : boolean
+// } //this is type aliases 
+
+// function createUser(user : User) : User {
+//     return {
+//         name: "",
+//         email: "",
+//         isActive : true
+//     }
+// }
+
+
+// // createUser({name: "" , email: "", isActive: true})
+
+
+//READONLY and Optional
+
 type User = {
+   readonly _id : number,
     name: string;
     email: string;
     isActive : boolean
-} //this is type aliases 
+}
 
-function createUser(user : User) : User {
-    return {
-        name: "",
-        email: "",
-        isActive : true
-    }
+let myuser : User = {
+  
+    _id: 1232,
+    name: "rushi",
+    email:"r@r.com",
+    isActive :false
 }
 
 
-createUser({name: "" , email: "", isActive: true})
+myuser.email = "rushi@gmail.com";
+// myuser._id = q3423  --> gives error cause we declear this one read only 
+
+type cardNumber = {
+    cardnumber : string
+
+}
+
+type cardDate = {
+   carddate : string
+}
+
+type cardDetails = cardNumber & cardDate & {
+  cvv : number
+} // HERE & is use to combine all this things toghter 
+
+
+let userAccount: cardDetails = {
+    cardnumber:"",
+    carddate: "",
+    cvv :2342,
+};
