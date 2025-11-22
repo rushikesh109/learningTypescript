d42c6619c2e2802f32ffb77372856f0644785297
Rushikesh Mangrule <you@example.com>
2025-11-22 20:07:10 +0530

    array

---
+++ 04/array.ts
@@ -0,0 +1,19 @@
+// const superHero: readonly string[] = [];
+const superHero:  string[] = [];
+const heroPower : number [] = []
+// const heroPower : Array<number> = []
+
+type Use = {
+    name: string
+    isActive: boolean 
+}
+
+const allUsers : User[] = []
+
+const MLModel: number[][] = [
+    [2232,2332,23232],
+    []
+]
+
+superHero.push("superman")
+heroPower.push(3);
