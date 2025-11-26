3a5c3bb86594897fd2d8c2afd968ca279852a684
Rushikesh Mangrule <you@example.com>
2025-11-26 17:16:59 +0530

    Public private

---
+++ 05_Purets/src/index.ts
@@ -0,0 +1,68 @@
+
+// class User {
+//     public email: string
+//     private name: string
+//     readonly city: string = "Jaipur"
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name
+
+//     }
+// }
+// class User {
+
+//     protected _courseCount = 1
+    
+//     readonly city: string = "Jaipur"
+//     constructor(
+//         public email: string, 
+//         public name: string,
+//         // private userId: string
+//         ){
+//     }
+//     private deleteToken(){
+//         console.log("Token deleted");
+        
+//     }
+
+//     get getAppleEmail(): string{
+//         return `apple${this.email}`
+//     }
+
+//     get courseCount(): number {
+//         return this._courseCount
+//     }
+
+//     set courseCount(courseNum) {
+//         if (courseNum <= 1) {
+//             throw new Error("Course count should be more than 1")
+//         }
+//         this._courseCount = courseNum
+//     }
+// }
+
+// class SubUser extends User {
+//     isFamily: boolean = true
+//     changeCourseCount(){
+//         this._courseCount = 4
+//     }
+// }
+
+// const hitesh = new User("h@h.com","hitesh")
+// hitesh.name 
+
+// hitesh.deleteToken()
+
+class User {
+    readonly city : string = "Pune"
+    constructor(
+        public email : string,
+        public name: string
+    ){
+ 
+        //another way to declare private public
+    }
+}
+
+
+
