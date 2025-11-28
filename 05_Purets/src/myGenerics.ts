de7d02950e040f1d5a3534047e6a09c72f517eaa
Rushikesh Mangrule <you@example.com>
2025-11-28 22:52:53 +0530

    all abt GENERICS

---
+++ 05_Purets/src/myGenerics.ts
@@ -0,0 +1,84 @@
+const score : Array<number> = []
+const names:Array<string> =[]
+
+function identityOne(val: boolean | number) : boolean | number{
+    return val;
+}
+
+function identityTwo(val: any) : any{
+    return val;
+}
+
+function identityThree<Type>(val: Type) : Type {
+    return val;
+}
+
+// identityThree(true)
+
+function identityFour<T>(val:T) : T{
+    return val;
+}
+
+//creating our own genericTypes
+
+interface Bottle{
+    brand : string,
+    type: number
+}
+
+// identityFour<Bottle> ({
+// })
+
+//ARROW FUNCTION USING GENERICS
+
+function getSearchProducts<T> (products: T[]) : T{
+    const myIndex = 3;
+    return products[myIndex]
+}
+
+const getMoreSearchProducts = <T,>(products: T[]): T => {
+    const myIndex = 4;
+    return products[myIndex]
+}
+
+interface dataBase {
+    connection : string,
+    username: string,
+    password: string
+}
+
+interface dataBase {
+    connection : string,
+    name: string,
+    password: string,
+}
+
+function anotherFunction <T, U extends dataBase> (valOne: T, valTwo: U): Object{
+    return{
+        valOne,
+        valTwo,
+    }
+}
+
+// anotherFunction("3", {
+   
+// })
+
+interface Quiz{
+    name: string,
+    type: String
+}
+
+interface Course{
+    name: string,
+    author: string,
+    subject : string,
+}
+
+class sellable<T,> {
+   public cart : T[] =[]
+
+   addToCart(product: T){
+    this.cart.push(product)
+   }
+}
